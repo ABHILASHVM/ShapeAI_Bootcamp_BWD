0fadadf56e9598fe2908a46fe2972dc95c7f6ff6
ABHILASHVM <abhilashvm44@gmail.com>
2021-06-26 20:10:25 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer()
+{
+  return (
+    <footer>
+      <div>
+  <p>
+  Copyright by ShapeAI @ {new Date().getFullYear()}
+  </p>
+  </div>
+  </footer>
+  );
+}
+
+export default Footer;
